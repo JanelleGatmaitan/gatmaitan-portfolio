c79d66e9aa29c5e38d3c75cb09bd3c7e5e59617b
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-08-30 14:40:53 -0700

    populate with data #8

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -3,9 +3,9 @@ import ContactIcons from './contact-icons';
 
 function About(props) {
   // const firstName = props.data.firstName;
-  const { firstName, lastName, contact } = props.data;
+  const { firstName, lastName, contact, bio, projects } = props.data;
+  console.log('bio: ', bio);
   const classes = `person-card ${firstName.toLowerCase()}`;
-  console.log("props.data.contacy: ", contact);
 
   return (
     <div className="row about" id="about">
@@ -22,9 +22,11 @@ function About(props) {
       <div className="col text">
         <div className="profile-text">
           <h1>{`Hello, I'm ${firstName}`}</h1>
-          <p className="about-text">Lorem ipsum dolor sit amet.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Turpis massa sed elementum tempus egestas sed sed risus. Leo duis ut diam quam nulla. Volutpat sed cras ornare arcu dui. Eu sem integer vitae justo eget magna fermentum iaculis eu. Sapien pellentesque habitant morbi tristique senectus et. Eu turpis egestas pretium aenean pharetra magna ac. Sed pulvinar proin gravida hendrerit. Nunc lobortis mattis aliquam faucibus purus in. Ornare lectus sit amet est. Eget mauris pharetra et ultrices neque ornare aenean euismod. Morbi non arcu risus quis. At imperdiet dui accumsan sit. Elementum integer enim neque volutpat ac tincidunt vitae semper.
-          </p>
+          {
+            bio.map((paragraph) => {
+              return <p className="about-text">{paragraph}</p>
+            })
+          }
         </div>
       </div>
     </div>
