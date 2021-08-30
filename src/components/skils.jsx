e9322e5407fc6837c840721ba022c43f2bc6cd2d
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-08-30 16:44:33 -0700

    skill card #10

--- src/components/skils.jsx
+++ src/components/skils.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 
 function Skills(props) {
+  const skills = props.skills;
   return (
-    <p>sdfsfsdf</p>
+    <div className="skills">
+      <h1>Skills</h1>
+      <div className="row">
+        {skills.map(skill => {
+          const devIcon = skill.toLowerCase();
+          return (
+            <div className="skill-card">
+              <i key={skills.indexOf(skill)} className={`devicon-${devIcon}-plain colored skill-icon`} />
+              <p>{skill}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
